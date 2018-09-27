17b005528aaf74049347e7443ca17e88946b639c
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2018-09-27 13:51:32 +0200

    Splittet build_gui op i to andre funktioner

--- view.py
+++ view.py
@@ -8,14 +8,20 @@ class View(tk.Frame):
         self.build_gui()
 
     def build_gui(self):
+        self.build_buttons()
+        self.build_canvas()
+
+    def build_buttons(self):
         left_frame = tk.Frame(self)
         left_frame.pack(side=tk.LEFT)
+
+        self.lblr1 = tk.Label(left_frame, text='r1')
+        self.lblr1.pack(fill=tk.X)
         
+    def build_canvas(self):
         right_frame = tk.Frame(self)
         right_frame.pack(side=tk.RIGHT)
 
-        self.C = tk.Canvas(right_frame,bg="Red",height=500, width=500)
+        self.C = tk.Canvas(right_frame, bg='Red', height=500, width=500)
         self.C.pack()
 
-        self.lblr1 = tk.Label(left_frame,text='r1')
-        self.lblr1.pack(fill=tk.X)   
