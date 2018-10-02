43f2d3d21092cbfc9856d6602e88421c9c828dbf
Simon <32448896+Minieren@users.noreply.github.com>
2018-10-02 15:42:30 +0200

    Tegn kurve

--- controller.py
+++ controller.py
@@ -4,10 +4,10 @@ class Controller():
     def __init__(self):
         pass
     
-    def get_curve(self, r1, r2, w1, w2, phi, rang=1000):
+    def get_curve(self, r1, r2, w1, w2, phi, rang, step):
         xy = []
         for t in range(rang):
-            x = r1 * sin(w1*float(t)/1000)
-            y = r2 * sin(w2 * float(t)/1000) + phi
+            x = r1 * sin(w1*t*step)
+            y = r2 * sin(w2*t*step) + phi
             xy.append((x, y))
-        return xy        
+        return xy
--- view.py
+++ view.py
@@ -1,9 +1,12 @@
 import tkinter as tk
+from controller import Controller
 
 class View(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
+
+        self.controller = Controller()
 
         self.build_gui()
 
@@ -11,44 +14,83 @@ class View(tk.Frame):
         right_frame = tk.Frame(self)
         right_frame.pack(side=tk.RIGHT)
 
-        self.C = tk.Canvas(right_frame, bg='Red', height=500, width=500)
-        self.C.pack()
+        self.canvas = tk.Canvas(right_frame, height=500, width=500)
+        self.canvas.pack()
 
         left_frame = tk.Frame(self)
         left_frame.pack(side=tk.LEFT)
 
         #Start button
-        self.B1 = tk.Button(left_frame, text="Start", command=self.start)
-        self.B1.grid(row=0, column=0, sticky=tk.N+tk.S+tk.W+tk.E)
+        start = tk.Button(left_frame, text="Start", command=self.start)
+        start.grid(row=0, column=0, sticky=tk.N+tk.S+tk.W+tk.E)
 
-        self.B2 = tk.Button(left_frame, text="Stop", command=self.stop)
-        self.B2.grid(row=0, column=1, sticky=tk.N+tk.S+tk.W+tk.E)
+        #Stop button
+        stop = tk.Button(left_frame, text="Stop", command=self.stop)
+        stop.grid(row=0, column=1, sticky=tk.N+tk.S+tk.W+tk.E)
+
         #Radius set 1
-        self.R1 = tk.Scale(left_frame, from_=0.0, to=3.0, resolution=0.20, orient=tk.HORIZONTAL, label="Radius 1")
-        self.R1.grid(row=1, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
+        self.radius1 = tk.Scale(left_frame, from_=100, to=250, resolution=10, command=self.redraw_curve, orient=tk.HORIZONTAL, label="Radius 1")
+        self.radius1.grid(row=1, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
 
         #Raduis set 2 
-        self.R2 = tk.Scale(left_frame, from_=0.0, to=3.0, resolution=0.20, orient=tk.HORIZONTAL, label="Radius 2")
-        self.R2.grid(row=2, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
+        self.radius2 = tk.Scale(left_frame, from_=100, to=250, resolution=10, command=self.redraw_curve, orient=tk.HORIZONTAL, label="Radius 2")
+        self.radius2.grid(row=2, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
 
         #Omega set 1 
-        self.V1 = tk.Scale(left_frame, from_=0.0, to=3.0, resolution=0.20, orient=tk.HORIZONTAL, label="Omega 1")
-        self.V1.grid(row=3, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
+        self.omega1 = tk.Scale(left_frame, from_=2.0, to=5.0, resolution=0.20, command=self.redraw_curve, orient=tk.HORIZONTAL, label="Omega 1")
+        self.omega1.grid(row=3, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
 
         #Omega set 2
-        self.W2 = tk.Scale(left_frame, from_=0.0, to=3.0, resolution=0.20, orient=tk.HORIZONTAL, label="Omega 2")
-        self.W2.grid(row=4, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
+        self.omega2 = tk.Scale(left_frame, from_=2.0, to=5.0, resolution=0.20, command=self.redraw_curve, orient=tk.HORIZONTAL, label="Omega 2")
+        self.omega2.grid(row=4, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
 
         #Phi
-        self.P2 = tk.Scale(left_frame, from_=0.0, to=3.0, resolution=0.20, orient=tk.HORIZONTAL, label="Phi")
-        self.P2.grid(row=5, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
+        self.phi = tk.Scale(left_frame, from_=0.0, to=3.0, resolution=0.20, command=self.redraw_curve, orient=tk.HORIZONTAL, label="Phi")
+        self.phi.grid(row=5, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
 
-        #Range(t)
-        self.lblR = tk.Label(left_frame, text='Range(t)')
-        self.lblR.grid(row=6, column=0, sticky=tk.N+tk.S+tk.W)
+        #Range
+        rangeLbl = tk.Label(left_frame, text='Punkter')
+        rangeLbl.grid(row=6, column=0, sticky=tk.N+tk.S+tk.W)
 
-        self.Box = tk.Spinbox(left_frame, from_=0, to=10000)
-        self.Box.grid(row=7, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
+        self.range = tk.Spinbox(left_frame, from_=100, to=1000)
+        self.range.grid(row=7, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
+
+        #Step
+        stepLbl = tk.Label(left_frame, text='Trin')
+        stepLbl.grid(row=8, column=0, sticky=tk.N+tk.S+tk.W)
+
+        self.step = tk.Spinbox(left_frame, from_=0.01, to=1)
+        self.step.grid(row=9, column=0, columnspan=2, sticky=tk.N+tk.S+tk.W+tk.E)
+
+    def redraw_curve(self, evt=None):
+        r1 = self.radius1.get()
+        r2 = self.radius2.get()
+        w1 = self.omega1.get()
+        w2 = self.omega2.get()
+        p = self.phi.get()
+        rang = self.range.get()
+        step = self.step.get()
+
+        try:
+            rang = int(rang)
+            step = float(step)
+        except:
+            return
+
+        coordinates = self.controller.get_curve(r1, r2, w1, w2, p, rang, step)
+
+        self.canvas.delete("all")
+
+        for i in range(0, rang-1):
+            coord1 = coordinates[i]
+            x1 = coord1[0] + 250
+            y1 = coord1[1] + 250
+
+            coord2 = coordinates[i+1]
+            x2 = coord2[0] + 250
+            y2 = coord2[1] + 250
+
+            self.canvas.create_line(x1, y1, x2, y2, fill="blue")
 
     def start(self):
         pass
